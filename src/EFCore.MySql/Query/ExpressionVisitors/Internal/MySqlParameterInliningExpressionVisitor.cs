177038261c794c8b32a5d3bdeba4f83268986370
Laurents Meyer <laucomm@gmail.com>
2023-09-26 14:30:02 +0200

    Workaround MySQL 8 engine crash at some later point, when using JSON_TABLE(@json, ..) with @json being a parameter, by directly inlining the parameter values in question into the generated SQL.

---
+++ src/EFCore.MySql/Query/ExpressionVisitors/Internal/MySqlParameterInliningExpressionVisitor.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) Pomelo Foundation. All rights reserved.
+// Licensed under the MIT. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.EntityFrameworkCore.Storage;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure.Internal;
+using Pomelo.EntityFrameworkCore.MySql.Query.Expressions.Internal;
+using Pomelo.EntityFrameworkCore.MySql.Query.ExpressionTranslators.Internal;
+
+namespace Pomelo.EntityFrameworkCore.MySql.Query.ExpressionVisitors.Internal;
+
+/// <summary>
+/// Inject parameter inlining expressions where parameters are not supported for some reason.
+/// </summary>
+public class MySqlParameterInliningExpressionVisitor : ExpressionVisitor
+{
+    private readonly IRelationalTypeMappingSource _typeMappingSource;
+    private readonly ISqlExpressionFactory _sqlExpressionFactory;
+    private readonly IReadOnlyDictionary<string, object> _parametersValues;
+    private readonly IMySqlOptions _options;
+
+    private bool _inJsonTableSourceParameterCall;
+
+    public MySqlParameterInliningExpressionVisitor(
+        IRelationalTypeMappingSource typeMappingSource,
+        ISqlExpressionFactory sqlExpressionFactory,
+        IMySqlOptions options,
+        IReadOnlyDictionary<string, object> parametersValues)
+    {
+        _typeMappingSource = typeMappingSource;
+        _sqlExpressionFactory = sqlExpressionFactory;
+        _parametersValues = parametersValues;
+        _options = options;
+    }
+
+    protected override Expression VisitExtension(Expression extensionExpression)
+        => extensionExpression switch
+        {
+            MySqlJsonTableExpression jsonTableExpression => VisitJsonTable(jsonTableExpression),
+            SqlParameterExpression sqlParameterExpression => VisitSqlParameter(sqlParameterExpression),
+            ShapedQueryExpression shapedQueryExpression => shapedQueryExpression.Update(
+                Visit(shapedQueryExpression.QueryExpression),
+                Visit(shapedQueryExpression.ShaperExpression)),
+            _ => base.VisitExtension(extensionExpression)
+        };
+
+    protected virtual Expression VisitJsonTable(MySqlJsonTableExpression jsonTableExpression)
+    {
+        var parentInJsonTableSourceParameterCall = _inJsonTableSourceParameterCall;
+        _inJsonTableSourceParameterCall = true;
+        var jsonExpression = (SqlExpression)Visit(jsonTableExpression.JsonExpression);
+        _inJsonTableSourceParameterCall = parentInJsonTableSourceParameterCall;
+
+        return jsonTableExpression.Update(
+            jsonExpression,
+            jsonTableExpression.Path,
+            jsonTableExpression.ColumnInfos);
+    }
+
+    protected virtual Expression VisitSqlParameter(SqlParameterExpression sqlParameterExpression)
+    {
+        if (_inJsonTableSourceParameterCall &&
+            !_options.ServerVersion.Supports.JsonTableImplementationUsingParameterAsSourceWithoutEngineCrash)
+        {
+            return new MySqlInlinedParameterExpression(
+                sqlParameterExpression,
+                _sqlExpressionFactory.Constant(
+                    _parametersValues[sqlParameterExpression.Name],
+                    sqlParameterExpression.TypeMapping));
+        }
+
+        return sqlParameterExpression;
+    }
+}
--- src/EFCore.MySql/Query/Internal/MySqlParameterBasedSqlProcessor.cs
+++ src/EFCore.MySql/Query/Internal/MySqlParameterBasedSqlProcessor.cs
@@ -15,7 +15,6 @@ namespace Pomelo.EntityFrameworkCore.MySql.Query.Internal
     public class MySqlParameterBasedSqlProcessor : RelationalParameterBasedSqlProcessor
     {
         private readonly IMySqlOptions _options;
-        private readonly MySqlSqlExpressionFactory _sqlExpressionFactory;
 
         public MySqlParameterBasedSqlProcessor(
             RelationalParameterBasedSqlProcessorDependencies dependencies,
@@ -23,7 +22,6 @@ namespace Pomelo.EntityFrameworkCore.MySql.Query.Internal
             IMySqlOptions options)
             : base(dependencies, useRelationalNulls)
         {
-            _sqlExpressionFactory = (MySqlSqlExpressionFactory)Dependencies.SqlExpressionFactory;
             _options = options;
         }
 
@@ -48,7 +46,13 @@ namespace Pomelo.EntityFrameworkCore.MySql.Query.Internal
                     .Visit(queryExpression);
             }
 
-            queryExpression = new MySqlHavingExpressionVisitor(_sqlExpressionFactory).Visit(queryExpression);
+            queryExpression = new MySqlHavingExpressionVisitor((MySqlSqlExpressionFactory)Dependencies.SqlExpressionFactory).Visit(queryExpression);
+
+            queryExpression = new MySqlParameterInliningExpressionVisitor(
+                Dependencies.TypeMappingSource,
+                Dependencies.SqlExpressionFactory,
+                _options,
+                parametersValues).Visit(queryExpression);
 
             // Run the compatibility checks as late in the query pipeline (before the actual SQL translation happens) as reasonable.
             queryExpression = new MySqlCompatibilityExpressionVisitor(_options).Visit(queryExpression);
